764a3612422f96950b4f0f1f5228807566ec82d7
Shreyas <rasaikarshreyas21@gmail.com>
2021-12-18 13:53:33 +0530

    Created navbar,news and newsitem components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
+      <div>
+        <NavBar />
+        <News />
       </div>
     );
   }
